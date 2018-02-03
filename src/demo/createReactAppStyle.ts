75b90f67e3889d6ff2e14bdf4a1b9353054a57f9
Jad Sarout <xananax@yelostudio.com>
2018-02-03 11:01:41 +0200

    createReactApp example styles

---
+++ src/demo/createReactAppStyle.ts
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { createTypeStyle, px, deg, em } from '../createTypeStyle'
+
+const { style, keyframes, makeComponent:setupMakeComponent, mount:setupMount } = createTypeStyle()
+
+export { style }
+
+export const mount = setupMount(React.createElement,ReactDOM.render)
+
+export const makeComponent = setupMakeComponent(React.createElement)
+
+
+export const appClassName = style({
+  textAlign: `center`
+})
+
+export const appLogoSpinClassName = keyframes({
+  'from': { transform: `rotate(${deg(0)})` },
+  'to': { transform: `rotate(${deg(360)})`}
+})
+
+export const appLogoClassName = style({
+  animationName: appLogoSpinClassName,
+  animationDuration: `2s`,
+  animationIterationCount: `infinite`,
+  animationTimingFunction: `linear`,
+  height: px(80)
+})
+
+export const appHeaderClassName = style({
+  backgroundColor: `#222`,
+  height: px(150),
+  padding: px(20),
+  color: `white`,
+})
+
+export const appTitleClassName = style({
+  fontSize: em(1.5),
+})
+
+export const appIntroClassName = style({
+  fontSize: `large`,
+})
