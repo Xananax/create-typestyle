29d71b86619362df98ae9d019aefbc4bc3a10794
Xananax <xananax@yelostudio.com>
2019-01-02 21:25:19 +0200

    Updates

---
+++ precache-manifest.7cb3e5347e6dabaeaa9cb1c9a80c9743.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d985d32b45869f6c2471",
+    "url": "/Xananax/create-typestyle/static/js/main.d985d32b.chunk.js"
+  },
+  {
+    "revision": "64632d254de12920d461",
+    "url": "/Xananax/create-typestyle/static/js/1.64632d25.chunk.js"
+  },
+  {
+    "revision": "439887d31ba0c1aee412",
+    "url": "/Xananax/create-typestyle/static/js/runtime~main.439887d3.js"
+  },
+  {
+    "revision": "14ce520b6900e9aae74a0da58f7a9505",
+    "url": "/Xananax/create-typestyle/index.html"
+  }
+];
